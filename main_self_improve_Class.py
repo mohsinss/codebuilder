affc95d2cf2aac5ed14b2334d7d4bf4a9814f48a
Mohsin <92655551+mohsinss@users.noreply.github.com>
2023-03-30 12:58:04 -0700

    openai keys replaced

--- main_self_improve_Class.py
+++ main_self_improve_Class.py
@@ -108,9 +108,9 @@ class GPT4AutoCoder:
                 self.save_generated_code(response, filename=update_filename)
                 print(f"\nAssistant: The improved code has been saved as '{update_filename}'.")
 
-# "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"
+# "<your_api_key>"
 if __name__ == "__main__":
-    api_key = "<your_api_key>"
+    api_key = "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"
     auto_coder = GPT4AutoCoder(api_key)
     auto_coder.run()
 
